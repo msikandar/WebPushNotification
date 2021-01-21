fb313dc2e4371b084b1cd2ba9043d88c692d4bda
mohammad.sikandar.work <i-mohammed.sikandar@semusi.com>
2021-01-21 11:39:16 +0530

    s

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
 self.addEventListener("push", function (e) {
   const options = {
-    body: "Notification by sikandar",
-    icon: "/images/d2.jpg",
+    image: "/images/d2.jpg",
   };
   e.waitUntil(self.registration.showNotification("Hero Image", options));
 });
