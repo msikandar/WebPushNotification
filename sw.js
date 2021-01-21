e4b49f9b7a15e4cc29efdb58fa8fe5095ac51391
mohammad.sikandar.work <i-mohammed.sikandar@semusi.com>
2021-01-21 12:18:27 +0530

    g

--- sw.js
+++ sw.js
@@ -1,6 +1,19 @@
 self.addEventListener("push", function (e) {
   const options = {
+    icon: "/images/d2.jpg",
     image: "/images/d3.jpg",
+    actions: [
+      {
+        action: "explore",
+        title: "Explore this new world",
+        icon: "images/checkmark.png",
+      },
+      {
+        action: "close",
+        title: "I don't want any of this",
+        icon: "images/xmark.png",
+      },
+    ],
   };
   e.waitUntil(self.registration.showNotification("Hero Image", options));
 });
