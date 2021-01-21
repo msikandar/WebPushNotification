67ebfb94d99a17d9f465be04be1d916d43357378
mohammad.sikandar.work <i-mohammed.sikandar@semusi.com>
2021-01-21 11:04:49 +0530

    update

--- sw.js
+++ sw.js
@@ -1,20 +1,6 @@
 self.addEventListener("push", function (e) {
-  var options = {
-    body: "This notification was generated from a push!",
-    icon: "/images/download.jpeg",
-    vibrate: [100, 50, 100],
-    data: {
-      dateOfArrival: Date.now(),
-      primaryKey: "2",
-    },
-    actions: [
-      {
-        action: "explore",
-        title: "Explore this new world",
-        icon: "images/checkmark.png",
-      },
-      { action: "close", title: "Close", icon: "images/xmark.png" },
-    ],
-  };
-  e.waitUntil(self.registration.showNotification("Hello world!", options));
+    const options = {
+        image: '/images/download.jpeg'
+      };
+  e.waitUntil(self.registration.showNotification("Hero Image", options));
 });
