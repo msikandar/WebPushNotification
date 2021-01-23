91dd63aa314d8f54b9e2bb5da2d3ce1c8e0f1e27
mohammad.sikandar.work <i-mohammed.sikandar@semusi.com>
2021-01-23 12:03:42 +0530

    image local

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("push", function (e) {
   const options = {
     icon: "/images/d2.jpg",
-    image: "https://i.pinimg.com/originals/8e/61/ff/8e61ff28302b6fef1057d1b5afb52d3f.png",
+    image: "/images/d3.jpg",
     actions: [
       {
         action: "explore",
